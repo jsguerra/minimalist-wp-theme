74d3c1c1793a09ced5a7b6362e402a04622dc63b
Jose Guerra <jose@gcostudios.com>
2018-04-11 15:29:36 +0100

    updated file

--- header.php
+++ header.php
@@ -25,24 +25,17 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'minimalist-wp' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<?php
+		the_custom_logo();
+		if ( is_front_page() && is_home() ) :
+			?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$minimalist_wp_description = get_bloginfo( 'description', 'display' );
-			if ( $minimalist_wp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $minimalist_wp_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		else :
+			?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+		endif; ?>
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'minimalist-wp' ); ?></button>
