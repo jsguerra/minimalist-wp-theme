6ab67989da2f033a9a7d0db7928509110f628ed2
Jose Guerra <jose@gcostudios.com>
2018-04-12 22:55:24 +0100

    updated file

--- header.php
+++ header.php
@@ -26,29 +26,27 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'minimalist-wp' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="container">
+		<?php
+		the_custom_logo();
+		if ( is_front_page() && is_home() ) :
+			?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif; ?>
+		else :
+			?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+		endif; ?>
 
-			<nav id="site-navigation" class="main-navigation">
-				<a class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span>H</span></a>
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-				?>
-			</nav><!-- #site-navigation -->
-		</div><!-- .container -->
+		<nav id="site-navigation" class="main-navigation">
+			<a class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span>H</span></a>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
