f93d20eb69940b0a34b7499d791ed58eb999b156
Jose Guerra <jose@gcostudios.com>
2018-04-13 17:52:48 +0100

    updated file

--- footer.php
+++ footer.php
@@ -15,16 +15,11 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'minimalist-wp' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'minimalist-wp' ), 'WordPress' );
-				?>
-			</a>
+			&copy; <?php echo date('Y'); ?> All Rights Reserved.
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'minimalist-wp' ), 'minimalist-wp', '<a href="https://joseguerra.uk">Jose Guerra</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'minimalist-wp' ), 'Minimalist WP', '<a href="https://joseguerra.uk">Jose Guerra</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
