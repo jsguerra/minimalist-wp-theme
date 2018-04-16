130f132e546de96e0181d1ff5ef6b45b6fdd49a8
Jose Guerra <jose@gcostudios.com>
2018-04-16 18:01:45 +0100

    updated file

--- header.php
+++ header.php
@@ -39,16 +39,12 @@
 		endif; ?>
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle menu-1" aria-controls="primary-menu" aria-expanded="false">
-				<span class="item"></span>
-				<span class="item"></span>
-				<span class="item"></span>
-			</button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'minimalist-wp' ); ?></button>
 			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
