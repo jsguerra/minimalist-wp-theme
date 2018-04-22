0721931ad6643268e97ea6fa02e33f7cc3a95711
Jose Guerra <jose@gcostudios.com>
2018-04-22 21:07:09 +0100

    updated file

--- js/navigation.js
+++ js/navigation.js
@@ -3,6 +3,7 @@ var closeBtn = document.querySelector('.closebtn');
 closeBtn.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector('.main-navigation').classList.remove('open-nav');
+    document.querySelector('.js').classList.remove('is-noscroll');
 }, false);
 
 var openBtn = document.querySelector('.openbtn');
@@ -10,4 +11,5 @@ var openBtn = document.querySelector('.openbtn');
 openBtn.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector('.main-navigation').classList.add('open-nav');
+    document.querySelector('.js').classList.add('is-noscroll');
 }, false);
