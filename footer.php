d970a3706b57fa7d7dfa9e98b32b71e2471622e3
Jose Guerra <jose@gcostudios.com>
2018-04-29 23:12:04 +0100

    removed default markup

--- footer.php
+++ footer.php
@@ -16,11 +16,11 @@
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			&copy; <?php echo date('Y'); ?> All Rights Reserved.
-			<span class="sep"> | </span>
+			<!-- <span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'minimalist-wp' ), 'Minimalist WP', '<a href="https://joseguerra.uk">Jose Guerra</a>' );
-				?>
+				?> -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
